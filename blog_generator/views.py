71f7e50369eb03c77b9440469db949e9573e667b
Buggie <flashy5986@gmail.com>
2025-08-13 15:01:55 +0100

    Update views, URLs, requirements, and templates

--- blog_generator/views.py
+++ blog_generator/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def user_login(request):
+    return render(request, 'login.html')
+
+def user_signup(request):
+    if request.method =='POST':
+        username= request.POST('username'),
+        Password= request.POST('Password'),
+        Email= request.POST('Email'),
+        RepeatPassword= request.POST('RepeatPassword')
+
+        if Password == RepeatPassword:
+            try:
+                user = User.objects.create_user(username,Email,Password)
+                user.save()
+                login(request,user)
+                return redirect('/')
+            except:
+                error_message ='Error creating account'
+                return render (request, 'signup.html', {'error_message' : 'error message'})
+            
+        else: 
+         
+         error_message ='Password do not match'   
+         return render(request, 'signup.html', {'error_message':error_message})
+    return render(request, 'signup.html')
+
+
+def user_logout(request):
+    pass
