ed30cbaff13a4baac618206a69f0376fd0cef17a
Buggie <flashy5986@gmail.com>
2025-09-22 14:09:13 +0100

    fix middleware elillipsis issue

--- ai_blog/settings.py
+++ ai_blog/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    ...
+    
 ]
 
 ROOT_URLCONF = 'ai_blog.urls'
